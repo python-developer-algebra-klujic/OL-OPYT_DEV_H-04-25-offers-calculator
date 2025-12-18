8916958d3e9bd44c2a0cfde0aad9af80014f11f3
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-12-18 19:31:45 +0100

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.db import models
 
-# Create your models here.
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=150,
                             null=False,
@@ -20,7 +20,32 @@ class Ingredient(models.Model):
                                      default=Decimal('0.00'),
                                      null=False,
                                      blank=True,
-                                     help_text='Osnovna cijena')
+                                     help_text='Osnovna cijena sastojka')
+    price_modificator = models.DecimalField(max_digits=5,
+                                            decimal_places=3,
+                                            default=Decimal('1.00'),
+                                            null=False,
+                                            blank=True,
+                                            help_text='Modifikator cijene')
+    total_price = models.DecimalField(max_digits=18,
+                                      decimal_places=3,
+                                      default=Decimal('0.00'),
+                                      null=True,
+                                      blank=True,
+                                      help_text='Ukupna cijena sastojka')
+
+    def __str__(self):
+        if self.name != '' and self.code != '':
+            return f'{self.name} ({self.code})'
+        else:
+            return f'{self.name}'
 
     class Meta:
         ordering = ['name', 'code']
+
+    def calculate_total_price(self):
+        self.total_price = Decimal(self.base_price) * Decimal(self.price_modificator)
+
+    def save(self, *args, **kwargs):
+        self.calculate_total_price()
+        super(Ingredient, self).save(*args, **kwargs)
